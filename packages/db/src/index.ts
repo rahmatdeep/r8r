f628b0c1edabdd326ca0522eae3fccaba7cb7a29
rahmatdeep <rahmatdeep@gmail.com>
2025-09-23 05:16:03 +0530

    bug fix

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-export type { JsonObject } from "@prisma/client/runtime/library.js";
+export type { JsonObject } from "@prisma/client/runtime/library";
 
 export const prisma = new PrismaClient();
